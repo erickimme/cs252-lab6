bb372477104b1ed21547f3146b3de69c45b802fc
Andrew <Andrew@pal-nat186-42-252.itap.purdue.edu>
2018-04-15 16:27:36 -0400

    check word button now switches turns, also restrics the user to click on blocks not bordering owned blocks. still has bug on cancel word that always reverts color to white

--- cell.js
+++ cell.js
@@ -7,12 +7,14 @@ function Cell(i, j, w, character) {
 
   this.turn = 0;
   this.reset = false;
+  this.belong = false;
   this.character = character;
+  this.color = "";
 }
 
 Cell.prototype.show = function() {
   stroke(0);
-  if (this.reset == false) {
+  if (this.reset == false && this.belong == false) {
     // fill in boxes (SETUP)
     if (this.j == 0) {
       // player 1 starting side
@@ -42,15 +44,15 @@ Cell.prototype.show = function() {
       rect(this.x, this.y, this.w, this.w);
       fill (0);
       text(this.character, this.x + this.w * 0.43, this.y + this.w - 20);
-      this.color = "lightBlue";
+      this.color = "lightRed";
     } else if (this.turn == 2) {
       fill('#fa8072');
       rect(this.x, this.y, this.w, this.w);
       fill (0);
       text(this.character, this.x + this.w * 0.43, this.y + this.w - 20);
-      this.color = "lightRed";
+      this.color = "lightBlue";
     }
-  } else {
+  } else if (this.reset == true && this.belong == false) {
     // if reset rows are bottom row
     if (this.j == 12) {
       fill('#FA5858');
@@ -73,7 +75,22 @@ Cell.prototype.show = function() {
       text(this.character, this.x + this.w * 0.43, this.y + this.w - 20);
       this.color = "white";
     }
-  } 
+  } else if (this.belong == true) {
+      // color in the block to blue or red depending on turn
+      if (this.turn == 2) {
+        fill('#FA5858');
+        rect(this.x, this.y, this.w, this.w);
+        fill (0);
+        text(this.character, this.x + this.w * 0.43, this.y + this.w - 20);
+        this.color = "blue";
+      } else if (this.turn == 1) {
+        fill('#58ACFA');
+        rect(this.x, this.y, this.w, this.w);
+        fill (0);
+        text(this.character, this.x + this.w * 0.43, this.y + this.w - 20);
+        this.color = "red";
+      }
+  }
 }
 
 Cell.prototype.contains = function(x, y) {
@@ -82,9 +99,15 @@ Cell.prototype.contains = function(x, y) {
 
 Cell.prototype.colorIn = function(turn) {
   this.reset = false;
+  this.belong = false;
   this.turn = turn;
 }
 
 Cell.prototype.resetColor = function() {
   this.reset = true;
+}
+
+Cell.prototype.darkenColor = function(turn) {
+  this.belong = true;
+  this.turn = turn;
 }
--- control.js
+++ control.js
@@ -11,13 +11,16 @@ var cols;
 var rows;
 var w = 50;
 var turn = 1; // player 1's turn first
+var word = ""; // word the player is forming
+var wordCoords = []; // indexes of the word the player is forming
+
 var alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
 	'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var frequencies = [10.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 
 	5.094, 7.966, 0.153, 0.772, 4.025, 2.406, 6.749, 8.507, 1.929, 0.095, 
 	5.987,6.327, 6.056, 2.758, 0.978, 2.360, 0.150, 1.974, 0.074];
 
-var cumFrequencies = [];
+var cumFrequencies = []; // populated in setup
 
 function setup() {
   createCanvas(551, 651);
@@ -39,7 +42,11 @@ function setup() {
     }
   }
 
-
+  // reset word, wordCoords, turn
+  word = "";
+  resetWordField();
+  wordCoords = [];
+  turn = 1;
 }
 
 // generate random char from alphabet using the frequencies array
@@ -86,14 +93,61 @@ function gameOver() {
 
 }
 
+// check if two blocks are adjacent in all 8 directions
+function isAdjacent(x1, y1, x2, y2) {
+	if (Math.abs(x1 - x2) > 1 || Math.abs(y1 - y2) > 1) {
+		return false;
+	}
+    var dx = Math.abs(x1 - x2);
+    var dy = Math.abs(y1 - y2);
+    return (dx + dy <= 2);
+}
+
+// check if already visited this block before for not
+function noOverlap(x, y) {
+	for (var i = 0; i < wordCoords.length; i++) {
+		var prev = wordCoords[i].split(" ");
+		var prevX = prev[0];
+		var prevY = prev[1];
+		if (x == prevX && y == prevY) {
+			return false;
+		}
+	}
+	return true;
+}
+
 function mousePressed() {
-  for (var i = 0; i < cols; i++) {
-    for (var j = 0; j < rows; j++) {
-      if (grid[i][j].contains(mouseX, mouseY)) {
-        grid[i][j].colorIn(turn);
-      }
-    }
-  }
+	for (var x = 0; x < cols; x++) {
+		for (var y = 0; y < rows; y++) {
+			var curr = grid[x][y];
+
+	    	if (grid[x][y].contains(mouseX, mouseY)) {
+		      	console.log(word);
+		      	if (word.length === 0) {
+		      		// if choosing first letter, must start at an already owned block
+		      		if ((turn == 2 && grid[x][y].color.localeCompare("blue") == 0) ||
+		      		 (turn == 1 && grid[x][y].color.localeCompare("red") == 0))  {
+		      			grid[x][y].colorIn(turn);
+		      			word += grid[x][y].character;
+		      			var coords = x + " " + y;
+		      			wordCoords.unshift(coords);
+		      		}
+		      	} else {
+		      		// if already chosen first letter, must choose adjacent block to first value in wordCoords
+		      		var recentCoords = wordCoords[0].split(" ");
+		      		if (isAdjacent(x, y, recentCoords[0], recentCoords[1]) && noOverlap(x, y)) {
+		      			console.log("here");
+		      			grid[x][y].colorIn(turn);
+		      			word += grid[x][y].character;
+		      			var coords = x + " " + y;
+		      			wordCoords.unshift(coords);
+		      		}
+		      	}
+
+		      	resetWordField();
+	      	}
+		}
+	}
 }
 
 function draw() {
@@ -107,15 +161,34 @@ function draw() {
 
 // check if word is valid and switch turns if user did enter valid
 function checkWord() {
+	// check if word is valid
+
+
+	// if valid, change color to dark version for the current color
+	for (var i = 0; i < wordCoords.length; i++) {
+		var coords = wordCoords[i].split(" ");
+		grid[coords[0]][coords[1]].darkenColor(turn);
+	}
+
+	// if not valid clear word
+
+	// switch user
 	if (turn == 1) {
 		turn = 2;
 	} else if(turn == 2){
 		turn = 1;
 	}
+
+	word = "";
+	wordCoords = [];
+	resetWordField();
 }
 
-// clear previously highlighted if user clicks clear word
+// clear previously highlighted if user clicks cancel word
  function clearWord() {
+ 	word = "";
+ 	wordCoords = [];
+ 	resetWordField();
  	for (var i = 0; i < rows; i++) {
  		for (var j = 0; j < cols; j++) {
  			var curr = grid[j][i];
@@ -126,24 +199,8 @@ function checkWord() {
  	}
  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ function resetWordField() {
+	var display = document.getElementById("currentWord");
+  	display.innerHTML = "\"" + word + "\"";
+  	display.setAttribute("class", "helveticaLarge wordPos");
+ }
